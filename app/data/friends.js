358e591d5f1af5d831ebc5a51f68cd71652970cf
Andrew <andrew.mpederson@gmail.com>
2018-10-30 16:52:42 -0600

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,73 @@
+// Data structure that will store the friends data
+var friends = [
+    {
+        name: "Andrew",
+        photo: "",
+        scores: [
+        "3",
+        "4",
+        "4",
+        "3",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "3"
+        ]
+    },
+  
+    {
+        name: "Alex",
+        photo: "",
+        scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "3",
+        "2",
+        "2",
+        "4",
+        "4",
+        "2"
+        ]
+    },
+  
+    {
+        name: "Melissa",
+        photo: "",
+        scores: [
+        "5",
+        "2",
+        "5",
+        "2",
+        "3",
+        "2",
+        "3",
+        "1",
+        "2",
+        "5"
+        ]
+    },
+  
+    {
+        name: "Wendy",
+        photo: "",
+        scores: [
+        "4",
+        "4",
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "2",
+        "5"
+        ]
+    }
+  
+  ];
+  
+  module.exports = friends;
